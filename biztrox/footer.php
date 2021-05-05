5a178c242a181954f7d1f03fdaf3d212cf15d9fe
SutharHet <76525657+SutharHet@users.noreply.github.com>
2021-05-05 11:13:26 +0530

    Add files via upload
    added config and biztrox outside

---
+++ biztrox/footer.php
@@ -0,0 +1,125 @@
+
+<!-- /client logo slider -->
+<!-- footer -->
+<footer class="bg-secondary">
+    <div class="py-100 border-bottom" style="border-color: #454547 !important">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <div class="mb-5 mb-md-0 text-center text-md-left">
+                        <!-- logo -->
+                        <h2 style="color:white;">CCMS</h2>
+                        <!-- social icon -->
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a class="social-icon-outline" href="#">
+                                    <i class="ti-facebook"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="social-icon-outline" href="#">
+                                    <i class="ti-twitter-alt"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="social-icon-outline" href="#">
+                                    <i class="ti-vimeo-alt"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="social-icon-outline" href="#">
+                                    <i class="ti-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- footer links -->
+                <div class="col-lg-2 col-md-4 col-6">
+                    <h4 class="text-white mb-4">Quick Link</h4>
+                    <ul class="footer-links">
+                        <li>
+                            <a href="about_us.php">About Us</a>
+                        </li>
+                        <li>
+                            <a href="contactus.php">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- subscribe form -->
+                <div class="col-lg-5 col-md-12 offset-lg-1">
+                    <div class="mt-4 mt-lg-0 text-center text-md-left">
+                        <h4 class="mb-5 text-white">Subscribe Us</h4>
+                        <p class="text-white mb-5">The main objective of CCMS is focus on issue related to complaint management in college.This is also developed to increase the communication efficiency between college staff and students</p>
+                        <form action="#" class="position-relative">
+                            <input type="text" class="form-control subscribe" name="subscribe" id="Subscribe" placeholder="Enter Your Email">
+                            <button class="btn-subscribe" type="submit" value="send">
+                                <i class="ti-arrow-right"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- copyright -->
+    <div class="pt-4 pb-3 position-relative">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-5">
+                    <p class="text-white text-center text-md-left">
+                        <span class="text-primary">CCMS</span> &copy; 2019 All Right Reserved </p>
+                </div>
+                <div class="col-lg-6 col-md-7">
+                    <ul class="list-inline text-center text-md-right">
+                        <li class="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
+                            <a class="font-secondary text-white" href="#">Legal</a>
+                        </li>
+                        <li class="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
+                            <a class="font-secondary text-white" href="#">Sitemap</a>
+                        </li>
+                        <li class="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
+                            <a class="font-secondary text-white" href="#">Privacy Policy</a>
+                        </li>
+                        <li class="list-inline-item ml-lg-3 my-lg-0 ml-2 my-2 ml-0">
+                            <a class="font-secondary text-white" href="#">Terms &amp; Conditions</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- back to top -->
+        <button class="back-to-top">
+            <a href="#"><i class="ti-angle-up"></i></a>
+        </button>
+    </div>
+</footer>
+<!-- /footer --> 
+
+<!-- jQuery -->
+<script src="plugins/jQuery/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<!-- magnific popup -->
+<script src="plugins/magnific-popup/jquery.magnific.popup.min.js"></script>
+<!-- slick slider -->
+<script src="plugins/slick/slick.min.js"></script>
+<!-- mixitup filter -->
+<script src="plugins/mixitup/mixitup.min.js"></script>
+<!-- Google Map -->
+<script src="../../../maps.googleapis.com/maps/api/js7402?key=AIzaSyBI14J_PNWVd-m0gnUBkjmhoQyNyd7nllA"></script>
+<script  src="plugins/google-map/gmap.js"></script>
+<!-- Syo Timer -->
+<script src="plugins/syotimer/jquery.syotimer.js"></script>
+<!-- aos -->
+<script src="plugins/aos/aos.js"></script>
+<!-- Main Script -->
+<script src="js/script.js"></script>
+
+</body>
+
+<!-- Mirrored from demo.themefisher.com/themefisher/biztrox/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 03 Feb 2019 09:25:08 GMT -->
+</html>
